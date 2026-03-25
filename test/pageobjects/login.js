70ba6c5c0b306ad798c641e9b2c9d0df64461db1
keonasuao8786 <keon.asuao8786@stu.mtec.edu>
2026-03-25 13:31:20 -0600

    Removed some stuff

--- test/pageobjects/login.js
+++ test/pageobjects/login.js
@@ -1,13 +1,7 @@
 import { $ } from '@wdio/globals'
 import Page from './page.js';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
     get inputUsername () {
         return $('#user-name');
     }
