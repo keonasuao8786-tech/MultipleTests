2a3235191a0199a9fc226d2d58ca40001efee3c0
keonasuao8786 <keon.asuao8786@stu.mtec.edu>
2026-03-26 13:55:49 -0600

    Still broken but About page is the second most frustrating part about this assignment

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,22 +1,34 @@
 import { browser, expect, $ } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.js'
 import SecurePage from '../pageobjects/secure.page.js'
-import securePage from '../pageobjects/secure.page.js'
 
 describe('Hamburger Menu Testing -', () => {
     it('should use the All Items button correctly', async () => {
-        await LoginPage.open()
+        await LoginPage.open();
 
-            await LoginPage.login(SecurePage.allUsernames[0], 'secret_sauce')
+            await LoginPage.login('standard_user', 'secret_sauce');
                 for (let item = 0; item < SecurePage.items.length; item++) {
-                    await expect(SecurePage.hamburgerMenu).toBeDisplayed()
-                    await SecurePage.items[item].click()
-                    await expect($('#back-to-products')).toExist()
-                    await SecurePage.hamburgerMenu.click()
-                    await SecurePage.allItemsButton.click()
-                    await expect($('.app_logo')).toBeDisplayed()
+                    await expect(SecurePage.hamburgerMenu).toBeDisplayed();
+                    await SecurePage.items[item].click();
+                    await expect($('#back-to-products')).toExist();
+                    await SecurePage.hamburgerMenu.click();
+                    await SecurePage.allItemsButton.click();
+                    await expect($('.app_logo')).toBeDisplayed();
 
                 }
-                await SecurePage.logout()
+                await SecurePage.logout();
     })
-})
+    it('should use the About button properly', async () => {
+        await LoginPage.open();
+        await LoginPage.login('standard_user', 'secret_sauce');
+        await SecurePage.hamburgerMenu.click();
+        await expect(SecurePage.aboutButton).toBeDisplayed();
+        await SecurePage.aboutButton.click();
+        await browser.pause(500);
+       // await expect($('.MuiStack-root.nav-left.css-122ck6t')).toBeDisplayed()
+        await browser.back();
+        await expect($('.app_logo')).toBeDisplayed();
+        await browser.pause(1000);
+        await SecurePage.logout();      
+    });
+});
--- wdio.conf.js
+++ wdio.conf.js
@@ -87,14 +87,14 @@ export const config = {
     // baseUrl: 'http://localhost:8080',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 100000,
+    waitforTimeout: 10000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
     connectionRetryTimeout: 120000,
     //
     // Default request retries count
-    connectionRetryCount: 5,
+    connectionRetryCount: 3,
     //
     // Test runner services
     // Services take over a specific job you don't want to take care of. They enhance
