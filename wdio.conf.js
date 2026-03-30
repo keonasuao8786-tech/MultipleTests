32ab97ac96d2ea0260008cb5b5597c929ac35a05
keonasuao8786 <keon.asuao8786@stu.mtec.edu>
2026-03-30 14:07:09 -0600

    FINALLY GOT IT WORKING :crying_emoji

--- wdio.conf.js
+++ wdio.conf.js
@@ -87,7 +87,7 @@ export const config = {
     // baseUrl: 'http://localhost:8080',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 100000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
@@ -129,7 +129,7 @@ export const config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 600000
     },
 
     //
